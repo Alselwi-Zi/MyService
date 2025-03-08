11c35ae72e14c80fba7ad88ae7d40329891d0f33
Zikomselwi <alselwi.ziko23.aa@gmail.com>
2025-03-08 23:23:32 +0300

    edit send request

--- MyService/Controllers/RequestController.cs
+++ MyService/Controllers/RequestController.cs
@@ -4,32 +4,47 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Threading.Tasks;
 using Domin.Entity;
+
 using Infarstuructre.Data;
 using Infarstuructre.ViewModel;
 using MyService.Resource;
+using Microsoft.AspNetCore.Identity;
 
 namespace MyService.Controllers
 {
     public class RequestController : Controller
     {
         private readonly MyServiceDbContext _context;
-
-        public RequestController(MyServiceDbContext context)
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        public RequestController(MyServiceDbContext context, SignInManager<ApplicationUser> sign)
         {
             _context = context;
+            _signInManager = sign;
         }
 
         // GET: Request/Create
         public async Task<IActionResult> SendRequest()
         {
+
             var viewModel = new RequestViewModel
             {
                 OrderDate = DateTime.Today
             };
-
-            await PopulateDropdownsAsync(viewModel);
+            if (_signInManager.IsSignedIn(User))
+            {
+                await PopulateDropdownsAsync(viewModel);
+            
             return View(viewModel);
-        }
+            }
+            else
+            {
+                return RedirectToAction("Login", "Account");
+            }
+            }
+        
+        
 
         // POST: Request/Create
         [HttpPost]
